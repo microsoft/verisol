0c46aa6f9129cf155642ff3d3b95b3ad9f7f320c
Shuvendu Lahiri <Shuvendu@microsoft.com>
2019-01-27 23:26:56 -0800

    adding an example with explicit getter and assignment to a state variable from a call

---
+++ Test/regressions/ExplicitGetters.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.24;
+
+// translation of explicit getter function for public state variables
+
+contract A {
+
+    uint public x;
+    
+    function x() public view returns (uint) {require(x > 0); return x;}
+}
+
+contract B {
+
+    uint public y;
+    
+    constructor(A a) {y = a.x(); assert (y > 0); }
+}
