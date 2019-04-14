fda844dc729b331d08e74cb222f1ca686cc33211
Shuvendu Lahiri <shuvendu@microsoft.com>
2019-04-14 14:26:02 -0700

    copy solc to the binaries folders that need it

--- Sources/SolToBoogieTest/Program.cs
+++ Sources/SolToBoogieTest/Program.cs
@@ -3,6 +3,7 @@ namespace SolToBoogieTest
 {
     using System;
     using System.IO;
+    using System.Reflection;
     using System.Runtime.InteropServices;
     using Microsoft.Extensions.Logging;
 
@@ -12,19 +13,20 @@ namespace SolToBoogieTest
         {
             if (args.Length < 2 || args.Length > 3)
             {
-                Console.WriteLine("Usage: SolToBoogieTest <path-to-corral> <workingdir> [<test-prefix>]");
+                Console.WriteLine("Usage: SolToBoogieTest <path-to-corral> <test-dir> [<test-prefix>]");
                 Console.WriteLine("\t: if <test-prefix> is specified, we only run subset of tests that have the string as prefix");
                 return;
             }
 
             string corralPath = args[0];
-            string workingDirectory = args[1];
+            string testDir = args[1];
             string testPrefix = args.Length >= 3 ? args[2] : ""; 
             string solcName = GetSolcNameByOSPlatform();
-            string solcPath = Path.Combine(workingDirectory, "Tool", solcName);
-            string testDir = Path.Combine(workingDirectory, "Test", "regressions");
-            string configDir = Path.Combine(workingDirectory, "Test", "config");
-            string recordsDir = Path.Combine(workingDirectory, "Test");
+            //string solcPath = Path.Combine(workingDirectory, "Tool", solcName);
+            string solcPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), solcName);
+            string regressionDir = Path.Combine(testDir, "regressions");
+            string configDir = Path.Combine(testDir, "config");
+            string recordsDir = Path.Combine(testDir);
 
             ILoggerFactory loggerFactory = new LoggerFactory().AddConsole(LogLevel.Information);
             ILogger logger = loggerFactory.CreateLogger("SolToBoogieTest.RegressionExecutor");
@@ -33,7 +35,7 @@ namespace SolToBoogieTest
             {
                 Console.WriteLine($"Warning!!! only running tests that have a prefix {testPrefix}....");
             }
-            RegressionExecutor executor = new RegressionExecutor(solcPath, corralPath, testDir, configDir, recordsDir, logger, testPrefix);
+            RegressionExecutor executor = new RegressionExecutor(solcPath, corralPath, regressionDir, configDir, recordsDir, logger, testPrefix);
             executor.BatchExecute();
         }
 
