e2e67e384b33b2b48fba67cb304d85e9d355e84c
garbervetsky <diego.garbervetsky@gmail.com>
2019-07-24 15:29:43 -0700

    Revert "Add basic support for bytes calldata" It will be included in another PR
    This reverts commit c619bfb9c8993eb8355153b343e4267bc5378789.

--- Sources/SolToBoogie/MapArrayHelper.cs
+++ Sources/SolToBoogie/MapArrayHelper.cs
@@ -92,10 +92,6 @@ namespace SolToBoogie
                 Match match = arrayRegex.Match(typeString);
                 return BoogieType.Int;
             }
-            else if(typeString=="bytes calldata")
-            {
-                return BoogieType.Ref;
-            }
             else
             {
                 throw new SystemException($"Unknown type string during InferKeyTypeFromTypeString: {typeString}");
@@ -113,10 +109,6 @@ namespace SolToBoogie
             {
                 Match match = arrayRegex.Match(typeString);
                 return InferExprTypeFromTypeString(match.Groups[1].Value);
-            }
-            else if (typeString == "bytes calldata")
-            {
-                return BoogieType.Ref;
             }
             else
             {
