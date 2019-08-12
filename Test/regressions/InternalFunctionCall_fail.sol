508f08504ee31ba5c47d063369d11f29df959b14
ellab123 <ellab@microsoft.com>
2019-08-12 14:15:37 -0700

    test modified

--- Test/regressions/InternalFunctionCall_fail.sol
+++ Test/regressions/InternalFunctionCall_fail.sol
@@ -16,10 +16,10 @@ contract InternalFunctionCall {
 
     function testInternalFunctionCall(uint par1, bool par2, address par3) public {
         uint res1 = fooInt(par1);
-        assert (res1 != par1 + 1);
+        assert (res1 == par1 + 1);
 		
 		bool res2 = fooBool(par2);
-		assert (res2 != !par2);
+		assert (res2 == !par2);
 		
 		address res3 = fooAddr(par3);
 		assert (res3 != par3);
