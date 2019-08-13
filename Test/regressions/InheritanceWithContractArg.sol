5d5a768ae1276fc592f2d63d83bcd399894a0002
ellab123 <ellab@microsoft.com>
2019-08-13 15:06:51 -0700

    assert added

--- Test/regressions/InheritanceWithContractArg.sol
+++ Test/regressions/InheritanceWithContractArg.sol
@@ -13,5 +13,5 @@ contract DerivedContract is BaseContract {
 
     uint b;
     function Foo(A a) public { b = 1;}
-    function Bar(A a) public {this.Foo(a);}
+    function Bar(A a) public {this.Foo(a); assert (b == 1);}
 }
