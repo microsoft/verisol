e3388398118661a6de51f9cb338abe677662f5d8
Shuvendu Lahiri <Shuvendu@microsoft.com>
2019-08-22 23:44:37 -0700

    missing file

---
+++ Sources/SolToBoogie/TranslatorFlags.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolToBoogie
+{
+    /// <summary>
+    /// A set of flags to control the translation
+    /// </summary>
+    public class TranslatorFlags
+    {
+        public TranslatorFlags()
+        {
+            NoSourceLineInfoFlag = false;
+            NoDataValuesInfoFlag = false;
+            NoAxiomsFlag = false;
+            NoUnsignedAssumesFlag = false;
+            GenerateInlineAttributes = true;
+        }
+        public bool NoSourceLineInfoFlag { get; set; }
+        public bool NoDataValuesInfoFlag{ get; set; }
+        public bool NoAxiomsFlag { get; set; }
+        public bool NoUnsignedAssumesFlag { get; set; }
+        public bool GenerateInlineAttributes{ get; set; }
+    }
+}
