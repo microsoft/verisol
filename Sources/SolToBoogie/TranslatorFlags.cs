e45df38903479d0cd6d6a1be5fab1a06abd15499
Shuvendu Lahiri <Shuvendu@microsoft.com>
2019-08-23 18:32:01 -0700

    documentation for flags

--- Sources/SolToBoogie/TranslatorFlags.cs
+++ Sources/SolToBoogie/TranslatorFlags.cs
@@ -18,11 +18,30 @@ namespace SolToBoogie
             NoHarness = false; 
             GenerateInlineAttributes = true;
         }
+        /// <summary>
+        /// Omit printing sourceFile/sourceLine information
+        /// </summary>
         public bool NoSourceLineInfoFlag { get; set; }
+        /// <summary>
+        /// Omit printing instrumentation to print data values in counterexample trace
+        /// </summary>
         public bool NoDataValuesInfoFlag{ get; set; }
+        /// <summary>
+        /// Do not print axioms (except for allocation)
+        /// </summary>
         public bool NoAxiomsFlag { get; set; }
+        /// <summary>
+        /// Do not emit x >= 0 for unsigned expressions x
+        /// </summary>
         public bool NoUnsignedAssumesFlag { get; set; }
+        /// <summary>
+        /// Omit generating Corral and Boogie harness
+        /// </summary>
         public bool NoHarness { get; set; }
+        /// <summary>
+        /// When true, add {:inline 1} attribute, 
+        /// can be expensive for recursive procedure analysis by Corral
+        /// </summary>
         public bool GenerateInlineAttributes{ get; set; }
     }
 }
