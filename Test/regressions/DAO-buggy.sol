d641ee4898d7716c8b140eab42431d0773d06733
Shuvendu Lahiri <Shuvendu@microsoft.com>
2019-09-12 16:45:50 -0700

    adding example with send/transfer

---
+++ Test/regressions/DAO-buggy.sol
@@ -0,0 +1,49 @@
+pragma solidity >=0.4.24<0.6.0;
+
+contract SimpleDAO {
+    mapping (address => uint) public credit;
+    constructor() public {
+    }
+    function donate(address to, uint amount) public {
+        credit[to] += amount;
+    }
+    function queryCredit(address to) public view returns (uint) {
+        return credit[to];
+    }
+    function withdraw() public {
+        uint oldBal = address(this).balance;
+        uint amount = credit[msg.sender];
+        if (amount > 0) {
+            msg.sender.transfer(amount));
+            credit[msg.sender] = 0;
+        }
+        uint bal = address(this).balance;
+        assert(bal == oldBal || bal == (oldBal - amount));
+    }
+}
+
+contract Mallory {
+    SimpleDAO public dao;
+    uint count;
+    constructor (address daoAddr) public {
+        count = 0;
+        dao = SimpleDAO(daoAddr);
+        require(dao.queryCredit(address(this)) == 0);
+        require(address(this).balance == 1);
+    }
+    function() external {
+        if (count < 3) {
+            count ++;
+            dao.withdraw();
+        }
+    }
+    function donate() public {
+        dao.donate(address(this), address(this).balance);
+    }
+    function getJackpot() public {
+        dao.withdraw();
+    }
+    function queryBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
