043ce75822fd71fbc31591a29775cfe2e4ee1bb3
Shuvendu Lahiri <Shuvendu@microsoft.com>
2019-09-19 01:16:06 -0700

    dao regression for transfer

---
+++ Test/regressions/DAO-fixed-transfer.sol
@@ -0,0 +1,48 @@
+pragma solidity >=0.4.24<0.6.0;
+
+contract SimpleDAO {
+    mapping (address => uint) public credit;
+    constructor() public {
+    }
+    function donate(address to, uint amount) payable public {
+        credit[to] += amount;
+    }
+    function queryCredit(address to) public view returns (uint) {
+        return credit[to];
+    }
+    function withdraw() public {
+        uint oldBal = address(this).balance; 
+        address payable sender = msg.sender;
+        uint balSender = msg.sender.balance; // translated OK
+        uint amount = credit[msg.sender];
+        if (amount > 0) {
+            credit[msg.sender] = 0;  // FIXed
+            sender.transfer(amount); // VeriSol bug #185 (can't handle msg.sender.transfer)
+        }
+        uint bal = address(this).balance;
+        assert(bal == oldBal || bal == (oldBal - amount));
+    }
+}
+
+contract Mallory {
+    SimpleDAO public dao;
+    uint count;
+    constructor (address daoAddr) public {
+        count = 0;
+        dao = SimpleDAO(daoAddr);
+        require(dao.queryCredit(address(this)) == 0);
+        require(address(this).balance == 1);
+    }
+    function () payable external {
+        if (count < 2) {
+            count ++;
+            dao.withdraw();
+        }
+    }
+    function donate() public {        
+        dao.donate(address(this), address(this).balance);  //should this not be a send(this.balance)? 
+    }
+    function getJackpot() public {
+        dao.withdraw();
+    }
+}
