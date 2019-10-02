ab8c3e7c4ba8c8aae78ccc715bc96baba5d31716
Shuvendu Lahiri <Shuvendu@microsoft.com>
2019-10-02 16:42:28 -0700

    no safemath

--- Test/regressions/ERC20-nosafemath.sol
+++ Test/regressions/ERC20-nosafemath.sol
@@ -163,7 +163,7 @@ contract ERC20 is IERC20 {
         require(recipient != address(0), "ERC20: transfer to the zero address");
 
         _balances[sender] = _balances[sender].sub(amount);
-        _balances[recipient] = _balances[recipient].add(amount);
+        _balances[recipient] = _balances[recipient] + amount; // nosafemath //_balances[recipient] = _balances[recipient].add(amount);
         emit Transfer(sender, recipient, amount);
     }
 
