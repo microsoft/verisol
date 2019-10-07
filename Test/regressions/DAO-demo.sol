b4a6d24f8e64fc3f334700e1bc3b9bc3bdbf4b7a
Shuvendu Lahiri <shuvendu@microsoft.com>
2019-10-06 21:43:47 -0700

    DAO demo fixed

--- Test/regressions/DAO-demo.sol
+++ Test/regressions/DAO-demo.sol
@@ -14,19 +14,18 @@ contract SimpleDAO {
         return credit[to];
     }
     function withdraw() public {
-        VeriSol.Ensures(!(VeriSol.Old(credit[msg.sender]) == 0) || address(this).balance == VeriSol.Old(address(this).balance));
+        VeriSol.Ensures (address(this).balance >= VeriSol.Old(address(this).balance - credit[msg.sender]));        
 
         uint oldBal = address(this).balance; 
         uint amount = credit[msg.sender];
         if (amount > 0) {
-            credit[msg.sender] = 0;  // fix
+            // credit[msg.sender] = 0;  // fix
             bool success;
             bytes memory status;
             (success, status) = msg.sender.call.value(amount)(""); 
             require(success);
-            // credit[msg.sender] = 0;  // bug
+            credit[msg.sender] = 0;  // bug
         }
        uint bal = address(this).balance;
-       VeriSol.Ensures (address(this).balance >= VeriSol.Old(address(this).balance - credit[msg.sender]));
     }
 }
