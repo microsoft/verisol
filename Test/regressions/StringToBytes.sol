405408c39d75c145f13db9f8f245338869daeb98
ellab123 <ellab@microsoft.com>
2020-01-27 13:00:23 -0800

    tests casting "bytes" type to "string" in function arguments

--- Test/regressions/StringToBytes.sol
+++ Test/regressions/StringToBytes.sol
@@ -3,13 +3,13 @@ pragma solidity >=0.4.24 <0.6.0;
 //VeriSol complains about "bytes" arg being called with "string" value
 contract Test {
  
-  string z; 
+  //string z; 
   constructor () public {
   }
   
   function foo(bytes memory x) public 
   {
-	z = "a";
+	//z = "a";
   }  
 
   function test() public 
@@ -20,8 +20,13 @@ contract Test {
 	 //foo(a);
 	 
 	 //Case #2:
-	 foo(new bytes(0));      //VeriSol translation error in TranslateNewStatement
+	 //foo(new bytes(0));      //VeriSol translation error in TranslateNewStatement
 	 
+	 //foo(0x);                //solc error
+	 foo(bytes(""));           //works
+	 //foo(0x0);                  //solc error
+	 
+	 //////////////////////////////////////////////////////////////
 	 //bytes memory a;
 	 //bytes is same as byte[]
 	 //assert (a.length == 0);
