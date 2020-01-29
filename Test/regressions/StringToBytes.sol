4b140b60ba8bb85ca3b0119986fd31d50eea1c4f
ellab123 <ellab@microsoft.com>
2020-01-28 16:46:25 -0800

    minor

--- Test/regressions/StringToBytes.sol
+++ Test/regressions/StringToBytes.sol
@@ -1,37 +1,37 @@
 pragma solidity >=0.4.24 <0.6.0;
 
-//VeriSol complains about "bytes" arg being called with "string" value
+//VeriSol complains about "bytes" arg being called with "string" value (uncomment line 18)
+//VeriSol translation error when "bytes" argument is called with new bytes(0)" (uncomment line 23)
 contract Test {
  
-  //string z; 
+  bytes z; 
+  uint[2] a;
+  uint[2] b;
   constructor () public {
   }
   
-  function foo(bytes memory x) public 
+  function foo(bytes memory x) public returns (bytes memory y)
   {
-	//z = "a";
+	y = x;
   }  
 
   function test() public 
   {
-	 //foo("");
+	 //foo("");           //VeriSol translation error
 	 //Case #1:
 	 //bytes memory a = new bytes(0);
-	 //foo(a);
-	 
+	 //foo(a); 
 	 //Case #2:
 	 //foo(new bytes(0));      //VeriSol translation error in TranslateNewStatement
 	 
 	 //foo(0x);                //solc error
-	 foo(bytes(""));           //works
-	 //foo(0x0);                  //solc error
+	 //foo(0x0);               //solc error
 	 
-	 //////////////////////////////////////////////////////////////
-	 //bytes memory a;
-	 //bytes is same as byte[]
-	 //assert (a.length == 0);
-	 //bytes memory b = delete a;
-	 //foo(a);	 
-	 //z = foo("");     //solc detects error
+	 z = foo(bytes(""));           //compiles
+	 //assert (z == bytes(""));      //solc error
+	 //assert (z == 0x0);             //solc error
+	 assert (z[0] == 0x0);
+	 //assert (z.length == 0);       //solc compiles, but corral crashes:
+									//"Invalid type for argument 0 in map select: int (expected: Ref)"
   }
 }
